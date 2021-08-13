3fc9f73b8e6a2ea755ebffa1da04c35d3b054cd9
dg12988 <doug.goldberg@me.com>
2021-08-13 10:56:38 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,24 @@
+import { fromEvent, Subject } from "rxjs";
+
 const loggedInSpan: HTMLElement = document.querySelector('span#logged-in');
 const loginButton: HTMLElement = document.querySelector('button#login');
 const logoutButton: HTMLElement = document.querySelector('button#logout');
 const printStateButton: HTMLElement = document.querySelector('button#print-state');
 
+//Shows differences between Regular Subject and Behavior Subject. Shows login state
+
+const isLoggedIn$ = new Subject<boolean>();
+
+fromEvent(loginButton, 'click').subscribe(() => isLoggedIn$.next(true));
+fromEvent(logoutButton, 'click').subscribe(() => isLoggedIn$.next(false));
+
+//Nav Bar
+isLoggedIn$.subscribe(
+  isLoggedIn => loggedInSpan.innerText = isLoggedIn.toString()
+);
+
+// Buttons
+isLoggedIn$.subscribe(isLoggedIn =>{
+  logoutButton.style.display = isLoggedIn ? 'block' : 'none';
+  loginButton.style.display = !isLoggedIn ? 'block' : 'none';
+});
