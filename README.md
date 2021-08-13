39d93b00f9749ede013cb25b5c715b2b5c06c0ca
dg12988 <doug.goldberg@me.com>
2021-08-13 10:45:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-jw-course-7f-behaviorsubject-pogkuv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-jw-course-7f-behaviorsubject-pogkuv)
---
+++ index.ts
@@ -0,0 +1,5 @@
+const loggedInSpan: HTMLElement = document.querySelector('span#logged-in');
+const loginButton: HTMLElement = document.querySelector('button#login');
+const logoutButton: HTMLElement = document.querySelector('button#logout');
+const printStateButton: HTMLElement = document.querySelector('button#print-state');
+
